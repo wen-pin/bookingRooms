8ae8c9f42acc71624b7fd6caa9597434f46a1e39
wen-pin <iwuewqaj951@gmail.com>
2022-11-24 22:52:42 +0800

    test Failed to load resource: the server responded with a status of 404

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -200,7 +200,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: app.router.options.base,
-      env: {"baseUrl":"http://localhost:3000"}
+      env: {}
     }
     // Only set once
 
