9b64b0284936bfc2aa45a097e9bb09d03bc9cb3c
wen-pin <iwuewqaj951@gmail.com>
2023-02-01 22:56:37 +0800

    fix: 依據API回傳數量顯示床的數量

--- components/room/Beds.vue
+++ components/room/Beds.vue
@@ -5,12 +5,17 @@
         {{ $t('住宿地點') }}
       </div>
 
-      <v-card outlined width="200px" height="120px" class="p-5 mt-5">
-        <div>
-          <SvgIcon :iconClass="'doubleBed'" class="mr-2" />
+      <v-card outlined width="200px" height="140px" class="p-5 mt-5">
+        <div v-if="pattern">
+          <SvgIcon
+            v-for="i in pattern.bed"
+            :key="i"
+            :iconClass="'doubleBed'"
+            class="mr-2"
+          />
         </div>
-        <div>{{ $t('臥室') }}</div>
-        <div v-if="pattern">{{ pattern.bedroom }}張 1.4 米寬雙人床</div>
+        <div class="mt-5 mb-1">{{ $t('臥室') }}</div>
+        <div v-if="pattern">{{ pattern.bed }}張 雙人床</div>
       </v-card>
     </div>
   </DivideBlock>
