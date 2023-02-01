4d7e749c4306a78db2ea2313837cca8c14a58638
wen-pin <iwuewqaj951@gmail.com>
2023-02-01 22:57:34 +0800

    fix: 空間介紹內容從後端回傳資料

--- components/room/Introduce.vue
+++ components/room/Introduce.vue
@@ -11,7 +11,12 @@
         </v-icon>
       </div>
 
-      <CardDialog :dialog="dialog" :title="'空間介紹'" @update="updateDialog">
+      <CardDialog
+        :width="800"
+        :dialog="dialog"
+        :title="'空間介紹'"
+        @update="updateDialog"
+      >
         <v-card-text class="black--text !text-base mt-5" v-html="convertTobr">
         </v-card-text>
       </CardDialog>
@@ -23,19 +28,20 @@
 export default {
   name: 'roomIntroduce',
 
-  props: {},
+  props: {
+    spaceInfo: {
+      types: String,
+    },
+  },
 
   data() {
     return {
       dialog: false,
-
-      content:
-        '在這間大自然包圍的浪漫房源中欣賞四周美景。\n日出、日落、滿天星星\n\n##嚴禁訪客##\n為提供來入住的房客，都能享有私人空間隱私\n謝絕訪客，無法配合者，請勿下訂，謝謝\n\n其他注意事項\n#可提供幫忙準備烤肉爐+瓦斯：400元/次 #寵物需先徵詢房東同意\n#包棟、寵物押金：3000元，再請貴賓入住當天另外準備，並於退房後無誤後退回<br />\n##嚴禁訪客##\n為提供來入住的房客，都能享有私人空間隱私\n謝絕訪客，無法配合者，請勿下訂，謝謝\n',
     }
   },
   computed: {
     convertTobr() {
-      let arr = this.content.split('')
+      let arr = this.spaceInfo.split('')
       return arr
         .map((item) => {
           return item === '\n' ? '<br />' : item
