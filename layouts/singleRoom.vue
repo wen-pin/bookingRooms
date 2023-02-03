2f1ca3e15d2910efddbe0f22886475b00b73c02e
wen-pin <iwuewqaj951@gmail.com>
2023-02-04 00:55:21 +0800

    add: 單一房間layout

---
+++ layouts/singleRoom.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <UiAppbar v-if="!$vuetify.breakpoint.xs" />
+
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'default',
+
+  data() {
+    return {}
+  },
+}
+</script>
