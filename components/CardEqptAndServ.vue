3a3a71d94c5b2cade74bf3103d91303c883c4abf
wen-pin <iwuewqaj951@gmail.com>
2023-02-04 11:16:30 +0800

    fix: icon 字體大小

--- components/CardEqptAndServ.vue
+++ components/CardEqptAndServ.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="divide-solid divide-y divide-slate-200 mt-5 ml-2 mr-7">
     <div class="flex items-center mb-5">
-      <SvgIcon :iconClass="svgTitle" class="mr-3" />
+      <SvgIcon :iconClass="svgTitle" :className="'size3'" class="mr-3" />
 
       <div>
         <div
-          class="text-body-1 black--text"
+          class="text-body-1 black--text text-base"
           :class="isSupply ? '' : 'line-through'"
         >
           {{ title }}
         </div>
-        <div class="text-caption grey--text text--darken-1">
+        <div class="grey--text text--darken-1 text-sm">
           {{ subtitle }}
         </div>
       </div>
