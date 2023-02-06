188a59cf328a66aa98c123b8242b1a8f8edaa846
wen-pin <iwuewqaj951@gmail.com>
2023-02-07 00:46:08 +0800

    fix: 預定房間頁面手機版UI

--- pages/book/stays/_id.vue
+++ pages/book/stays/_id.vue
@@ -1,119 +1,180 @@
 <template>
-  <v-container class="!max-w-[1280px]">
-    <UiAppbarBook />
-
-    <div class="flex my-10">
-      <v-btn icon class="mr-6" @click="$router.go(-1)">
-        <v-icon> mdi-less-than </v-icon>
-      </v-btn>
-      <div class="text-4xl font-semibold">申請預訂</div>
-    </div>
-
-    <v-row class="px-[80px]">
-      <v-col cols="6">
-        <BookTrip
-          :limitPeople="room.limitPeople"
-          :isAcceptPet="room.isAcceptPet"
-        />
-
-        <div v-if="$auth.loggedIn" class="mt-8">
-          <BookPayment />
-
-          <BookUnsubscribe />
-
-          <div class="my-8 text-xs">
-            點選以下按鈕即表示本人同意房東的《房屋守則》、房客基本守則、《重新預訂和退款政策》，且允許
-            Airbnb 在我應負責賠償損壞時，可以向我的付款方式扣款。
-          </div>
-
-          <v-btn
-            :disabled="this.select.title === undefined"
-            width="150px"
-            height="50px"
-            color="#EC407A"
-            class="rounded-lg"
-          >
-            <span class="text-lg text-white" @click="payment()">
-              {{ $t('確認並付款') }}
-            </span>
+  <div>
+    <v-app-bar
+      v-if="$vuetify.breakpoint.xs"
+      app
+      color="white"
+      elevate-on-scroll
+    >
+      <div class="!relative font-semibold flex items-center h-full">
+        <div>
+          <v-btn icon @click="$router.go(-1)">
+            <v-icon small color="black"> mdi-less-than </v-icon>
           </v-btn>
 
-          <v-dialog v-model="successDialog" width="450" persistent>
-            <v-card height="500px" class="p-5">
-              <div class="flex flex-col items-center">
-                <SvgIcon
-                  :iconClass="'success'"
-                  :className="'success'"
-                  class="my-10"
-                />
-
-                <div class="text-3xl">預訂完成</div>
-
-                <v-btn
-                  color="#0288D1"
-                  width="300px"
-                  height="50px"
-                  dark
-                  depressed
-                  class="mt-[100px]"
-                  nuxt
-                  to="/trips"
-                >
-                  查看訂單詳情
-                </v-btn>
-
-                <v-btn
-                  width="300px"
-                  height="50px"
-                  dark
-                  depressed
-                  nuxt
-                  to="/"
-                  class="mt-5"
-                >
-                  回首頁
-                </v-btn>
-              </div>
-            </v-card>
-          </v-dialog>
+          <div
+            class="absolute top-[50%] left-[50%]"
+            style="transform: translate(-50%, -50%) !important"
+          >
+            申請預訂
+          </div>
         </div>
+      </div>
+    </v-app-bar>
 
-        <div v-else class="mt-10">
-          <div class="text-2xl font-medium">登入或註冊即可預訂</div>
+    <v-container class="!max-w-[1280px]">
+      <div v-if="$vuetify.breakpoint.xs" class="">
+        <BookRoomPhoto
+          :rentalType="room.rentalType"
+          :title="room.title"
+          :averageRating="room.averageRating"
+          :allMessages="room.allMessages"
+          :roomId="roomId"
+          class="px-6"
+        />
+      </div>
 
-          <v-tabs v-model="tab" fixed-tabs>
-            <v-tabs-slider color="light-blue darken-4"></v-tabs-slider>
+      <div v-else class="flex my-10">
+        <v-btn icon class="mr-6" @click="$router.go(-1)">
+          <v-icon> mdi-less-than </v-icon>
+        </v-btn>
+        <div class="text-4xl font-semibold">申請預訂</div>
+      </div>
 
-            <v-tab v-for="item in items" :key="item">
-              {{ item }}
-            </v-tab>
-          </v-tabs>
-
-          <v-tabs-items v-model="tab" class="mt-5">
-            <v-tab-item>
-              <FormLoginBook />
-            </v-tab-item>
-
-            <v-tab-item>
-              <FormRegisterBook />
-            </v-tab-item>
-          </v-tabs-items>
-        </div>
-      </v-col>
-
-      <v-col cols="6">
-        <div class="flex justify-end">
-          <BookCardPrice
-            :rentalType="room.rentalType"
-            :title="room.title"
+      <v-row :class="$vuetify.breakpoint.xs ? '' : 'px-[80px]'">
+        <v-col :cols="$vuetify.breakpoint.xs ? 12 : 6">
+          <BookTrip
+            :limitPeople="room.limitPeople"
+            :isAcceptPet="room.isAcceptPet"
             :averageRating="room.averageRating"
-            :allMessages="room.allMessages"
+            :location="room.location"
             :price="room.price"
           />
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+
+          <div
+            v-if="$vuetify.breakpoint.xs"
+            class="px-6 pb-6 border border-t-8 border-r-0 border-b-0 border-l-0 border-solid border-gray-200"
+          >
+            <BookCardPrice :price="room.price" />
+          </div>
+
+          <div
+            v-if="$auth.loggedIn"
+            class="mt-8"
+            :class="
+              $vuetify.breakpoint.xs
+                ? 'border border-t-8 border-r-0 border-b-0 border-l-0 border-solid border-gray-200'
+                : ''
+            "
+          >
+            <BookPayment />
+
+            <BookUnsubscribe />
+
+            <div class="my-8 text-xs">
+              點選以下按鈕即表示本人同意房東的《房屋守則》、房客基本守則、《重新預訂和退款政策》，且允許
+              Airbnb 在我應負責賠償損壞時，可以向我的付款方式扣款。
+            </div>
+
+            <v-btn
+              :disabled="this.select.title === undefined"
+              width="150px"
+              height="50px"
+              color="#EC407A"
+              class="rounded-lg"
+            >
+              <span class="text-lg text-white" @click="payment()">
+                {{ $t('確認並付款') }}
+              </span>
+            </v-btn>
+
+            <v-dialog v-model="successDialog" width="450" persistent>
+              <v-card height="500px" class="p-5">
+                <div class="flex flex-col items-center">
+                  <SvgIcon
+                    :iconClass="'success'"
+                    :className="'success'"
+                    class="my-10"
+                  />
+
+                  <div class="text-3xl">預訂完成</div>
+
+                  <v-btn
+                    color="#0288D1"
+                    width="300px"
+                    height="50px"
+                    dark
+                    depressed
+                    class="mt-[100px]"
+                    nuxt
+                    to="/trips"
+                  >
+                    查看訂單詳情
+                  </v-btn>
+
+                  <v-btn
+                    width="300px"
+                    height="50px"
+                    dark
+                    depressed
+                    nuxt
+                    to="/"
+                    class="mt-5"
+                  >
+                    回首頁
+                  </v-btn>
+                </div>
+              </v-card>
+            </v-dialog>
+          </div>
+
+          <div
+            v-else
+            :class="
+              $vuetify.breakpoint.xs
+                ? 'border border-t-8 border-r-0 border-b-0 border-l-0 border-solid border-gray-200 p-6'
+                : 'mt-10'
+            "
+          >
+            <div class="text-2xl font-medium">登入或註冊即可預訂</div>
+
+            <v-tabs v-model="tab" fixed-tabs>
+              <v-tabs-slider color="light-blue darken-4"></v-tabs-slider>
+
+              <v-tab v-for="item in items" :key="item">
+                {{ item }}
+              </v-tab>
+            </v-tabs>
+
+            <v-tabs-items v-model="tab" class="mt-5">
+              <v-tab-item>
+                <FormLoginBook />
+              </v-tab-item>
+
+              <v-tab-item>
+                <FormRegisterBook />
+              </v-tab-item>
+            </v-tabs-items>
+          </div>
+        </v-col>
+
+        <v-col v-if="!$vuetify.breakpoint.xs" cols="6">
+          <div class="flex justify-end">
+            <v-card outlined class="rounded-lg p-6" max-width="480px">
+              <BookRoomPhoto
+                :rentalType="room.rentalType"
+                :title="room.title"
+                :averageRating="room.averageRating"
+                :allMessages="room.allMessages"
+                :roomId="roomId"
+              />
+              <BookCardPrice :price="room.price" />
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -153,6 +214,9 @@ export default {
     },
     dateRangeText() {
       return this.$store.getters.dateRangeText
+    },
+    roomId() {
+      return this.$route.params.id
     },
   },
   methods: {
