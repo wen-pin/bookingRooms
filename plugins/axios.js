2f6fcb9893cf2fefde497303e6db00bf9d442bda
wen-pin <iwuewqaj951@gmail.com>
2023-02-08 15:17:00 +0800

    add: axios插件 401 409錯誤顯示提示

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 // https://axios.nuxtjs.org/helpers/#setheader-name-value-scopes-common
-export default function ({ $axios, redirect, $auth }) {
+export default function ({ $axios, redirect, $auth, $toast }) {
   $axios.onRequest((config) => {
     console.log('Making request to ' + config.url)
 
@@ -16,9 +16,23 @@ export default function ({ $axios, redirect, $auth }) {
   $axios.onResponse((response) => {})
 
   $axios.onError((error) => {
-    if (error.response.status === 401) {
-      console.log('需要登入才能看到')
-      // redirect('/')
+    // const code = parseInt(error.response && error.response.status)
+    // if (code === 401) {
+    //   console.log('需要登入才能看到')
+    //   $toast.error('需要登入才能看到')
+    //   redirect('/401')
+    // }
+  })
+
+  $axios.onResponseError((error) => {
+    const code = parseInt(error.response && error.response.status)
+
+    if (code === 401) {
+      $toast.error('帳號或密碼錯誤')
+    }
+
+    if (code === 409) {
+      $toast.error('帳號已重複，請更改帳號')
     }
   })
 }
